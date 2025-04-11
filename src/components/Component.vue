3b35fe65c44cf1845c13b98cde169ea8ce38610e
aalexandramarta <r0972487@student.thomasmore.be>
2025-04-11 17:07:12 +0200

    Initial setup

---
+++ src/components/Component.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Component",
+        props: {
+            fn: {
+                type: String,
+                required: true
+            }
+        },
+        data(){
+            return {
+            }
+        },
+        methods: {
+            
+        }
+    }
+</script>
+
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
+
+//if we need new packeges you have to do it here
